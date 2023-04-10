43f8caf9fe0884dfe0029ef4d581c6b35edfa0e7
SanaShafique <81868664+SanaShafique@users.noreply.github.com>
2023-04-10 13:04:18 +0500

    Commit1

---
+++ mad_smsterproject/lib/screens/auth/login_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:mad_smsterproject/main.dart';
+import 'package:mad_smsterproject/screens/home_screen.dart';
+
+class loginscreen extends StatefulWidget {
+  const loginscreen({super.key});
+
+  @override
+  State<loginscreen> createState() => _loginscreenState();
+}
+
+class _loginscreenState extends State<loginscreen> {
+  @override
+  bool _isanimate = false;
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _isanimate = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    mq = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Welcome to Chattybuzz'),
+      ),
+      body: Stack(children: [
+        //app logo
+        AnimatedPositioned(
+            top: mq.height * .15,
+            right: _isanimate ? mq.width * .25 : -mq.width * .5,
+            width: mq.width * .5,
+            duration: const Duration(seconds: 1),
+            child: Image.asset('assets/icon.jpeg')),
+
+        //google login button
+        Positioned(
+            bottom: mq.height * .15,
+            left: mq.width * .05,
+            width: mq.width * .9,
+            height: mq.height * .06,
+            child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 223, 255, 187),
+                    shape: const StadiumBorder(),
+                    elevation: 1),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (_) => homescreen()));
+                },
+
+                //google icon
+                icon: Image.asset('assets/google.jpg', height: mq.height * .03),
+
+                //login with google label
+                label: RichText(
+                  text: const TextSpan(
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                      children: [
+                        TextSpan(text: 'Login with '),
+                        TextSpan(
+                            text: 'Google',
+                            style: TextStyle(fontWeight: FontWeight.w500)),
+                      ]),
+                ))),
+      ]),
+    );
+  }
+}
---
+++ mad_smsterproject/lib/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class homescreen extends StatefulWidget {
+  const homescreen({super.key});
+
+  @override
+  State<homescreen> createState() => _homescreenState();
+}
+
+class _homescreenState extends State<homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(CupertinoIcons.home),
+        title: const Text('Chattybuzz'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(
+            onPressed: () {}, child: const Icon(Icons.add_comment_rounded)),
+      ),
+    );
+  }
+}
